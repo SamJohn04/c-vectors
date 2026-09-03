998b41421e4ce949bfbc9bd5649dd29aa0d6070a
Samuel John <samuel.john.codes@gmail.com>
2026-09-03 20:34:29 +0530

    feat!: make vector_remove_last and vector_remove functions; test: add tests for the new functions

--- src/testvectors.c
+++ src/testvectors.c
@@ -1,16 +1,17 @@
-#include <assert.h>
-#include <stdio.h>
 #include "vectors.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-bool test_basics(const char* end_of_line);
-bool test_functions(const char* end_of_line);
+bool test_basics(const char *end_of_line);
+bool test_functions(const char *end_of_line);
 
-int* get_vector(int);
-bool put_vector(int* vector);
-void append_to_vector(int* vector, int);
+int *get_vector(int);
+bool put_vector(int *vector);
+void append_to_vector(int *vector, int);
 
 int main() {
-  char* end_of_line = "\n\t";
+  char *end_of_line = "\n\t";
 
   printf("\nTests: Basics\n\t");
   test_basics(end_of_line);
@@ -19,29 +20,29 @@ int main() {
   test_functions(end_of_line);
 }
 
-bool test_basics(const char* end_of_line) {
+bool test_basics(const char *end_of_line) {
   const int max_size = 2;
 
-  printf("test: vector_create_with_capacity          \t");
-  double* n = NULL;
+  printf("test: vector_create_with_capacity                    \t");
+  double *n = NULL;
   vector_create_with_capacity(n, max_size);
   assert(n != NULL && "vector after create is still NULL");
   printf("[SUCCESS]%s", end_of_line);
 
-  printf("test: vector_append                        \t");
+  printf("test: vector_append                                  \t");
   vector_append(n, 1.5);
   printf("[SUCCESS]%s", end_of_line);
 
-  printf("test: vector_len                           \t");
+  printf("test: vector_len                                     \t");
   assert(vector_len(n) == 1 && "vector_len is not 1");
   printf("[SUCCESS]%s", end_of_line);
 
-  printf("test: vector_free                          \t");
+  printf("test: vector_free                                    \t");
   vector_free(n);
   printf("[SUCCESS]%s", end_of_line);
 
-  printf("test: capacity is increased if it overflows\t");
-  int* n1;
+  printf("test: capacity is increased if it overflows          \t");
+  int *n1;
   vector_create_with_capacity(n1, max_size);
   vector_append(n1, 1);
   vector_append(n1, 2);
@@ -50,14 +51,14 @@ bool test_basics(const char* end_of_line) {
 
   vector_free(n1);
 
-  printf("test: append with NULL vector              \t");
-  int* n2 = NULL;
+  printf("test: append with NULL vector                        \t");
+  int *n2 = NULL;
   vector_append(n2, 1);
   vector_append(n2, 2);
   vector_append(n2, 3);
   printf("[SUCCESS]%s", end_of_line);
 
-  printf("test: access elements like an array        \t");
+  printf("test: access elements like an array                  \t");
   int sum = 0;
   for (int i = 0; i < vector_len(n2); i++) {
     sum += n2[i];
@@ -65,12 +66,29 @@ bool test_basics(const char* end_of_line) {
   assert(sum == 6 && "sum of elements is not 6");
   printf("[SUCCESS]%s", end_of_line);
 
+  printf("test: remove an element from the end of the array    \t");
+  vector_remove_last(n2);
+  assert(vector_len(n2) == 2 && "lens do not match");
+  assert(n2[0] == 1 && n2[1] == 2 && "values do not match");
+  printf("[SUCCESS]%s", end_of_line);
+
+  printf("test: remove an element from the middle of the array \t");
+  vector_append(n2, 3);
+  vector_append(n2, 4);
+  vector_append(n2, 5);
+
+  vector_remove(n2, 2); // remove the 3rd element
+  assert(vector_len(n2) == 4 && "lens do not match");
+  assert(n2[0] == 1 && n2[1] == 2 && n2[2] == 4 && n2[3] == 5 &&
+         "values do not match");
+  printf("[SUCCESS]%s", end_of_line);
+
   vector_free(n2);
 }
 
-bool test_functions(const char* end_of_line) {
+bool test_functions(const char *end_of_line) {
   printf("test: calling a function which returns a vector   \t");
-  int* v = get_vector(4);
+  int *v = get_vector(4);
   assert(v != NULL && "returned vector is null");
   printf("[SUCCESS]%s", end_of_line);
 
@@ -79,12 +97,10 @@ bool test_functions(const char* end_of_line) {
   printf("[SUCCESS]%s", end_of_line);
 }
 
-int* get_vector(int n) {
-  int* vec = NULL;
+int *get_vector(int n) {
+  int *vec = NULL;
   vector_create_with_capacity(vec, n);
   return vec;
 }
 
-bool put_vector(int* vector) {
-  return vector != NULL;
-}
+bool put_vector(int *vector) { return vector != NULL; }
--- src/vectors.h
+++ src/vectors.h
@@ -8,8 +8,8 @@
 #include <stdlib.h>
 
 typedef struct {
-    size_t length;
-    size_t capacity;
+  size_t length;
+  size_t capacity;
 } _vct_header_t;
 
 #define VECTOR_INITIAL_CAPACITY 256
@@ -19,12 +19,12 @@ typedef struct {
 // If you're fine with not choosing an initial capacity yourself,
 // just call vector_append directly
 #define vector_create_with_capacity(vector, max_size)                                       \
-    do {                                                                                    \
+  do {                                                                                      \
     _vct_header_t* header = malloc(sizeof(_vct_header_t) + sizeof(*(vector)) * (max_size)); \
     header->length = 0;                                                                     \
     header->capacity = max_size;                                                            \
     vector = (void*)(header+1);                                                             \
-    } while(0)
+  } while(0)
 
 // Get the length of a vector
 // Must give the start of the vector
@@ -32,20 +32,32 @@ typedef struct {
 
 // Append an element to the vector
 // Must give the start of the vector and the element
-#define vector_append(vector, element)                                                                           \
-    do {                                                                                                         \
-        if ((vector) == NULL) {                                                                                  \
-            vector_create_with_capacity(vector, VECTOR_INITIAL_CAPACITY);                                        \
-        }                                                                                                        \
-        _vct_header_t* header = ((_vct_header_t*)(vector) - 1);                                                  \
-        header->length++;                                                                                        \
-        if (header->length >= header->capacity) {                                                                \
-            header->capacity *= 2;                                                                               \
-            header            = realloc(header, sizeof(_vct_header_t) + sizeof(*(vector)) * (header->capacity)); \
-            vector            = (void*)(header+1);                                                               \
-        }                                                                                                        \
-        (vector)[header->length - 1] = (element);                                                                \
-    } while (0)
+#define vector_append(vector, element)                                                                     \
+  do {                                                                                                     \
+    if ((vector) == NULL) {                                                                                \
+      vector_create_with_capacity(vector, VECTOR_INITIAL_CAPACITY);                                        \
+    }                                                                                                      \
+    _vct_header_t* header = ((_vct_header_t*)(vector) - 1);                                                \
+    header->length++;                                                                                      \
+    if (header->length >= header->capacity) {                                                              \
+      header->capacity *= 2;                                                                               \
+      header            = realloc(header, sizeof(_vct_header_t) + sizeof(*(vector)) * (header->capacity)); \
+      vector            = (void*)(header+1);                                                               \
+    }                                                                                                      \
+    (vector)[header->length - 1] = (element);                                                              \
+  } while (0)
+
+// Remove the last element of the vector *without* returning it
+#define vector_remove_last(vector) ((_vct_header_t*)(vector) - 1)->length--
+
+// Remove any element of the vector *without* returning it
+#define vector_remove(vector, index)                         \
+  do {                                                       \
+    for (int i = (index); i < vector_len(vector) - 1; i++) { \
+      (vector)[i] = (vector)[i+1];                           \
+    }                                                        \
+    vector_remove_last(vector);                              \
+  } while (0)
 
 // free the vector
 // Must give the start of the vector
