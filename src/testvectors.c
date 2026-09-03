5af8738a579a015e4a0bfd0e3195a2581335cd23
Samuel John <samuel.john.codes@gmail.com>
2026-09-03 21:46:10 +0530

    test: update test to work with colors and different wordings

--- src/testvectors.c
+++ src/testvectors.c
@@ -1,45 +1,60 @@
 #include "vectors.h"
-#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-bool test_basics(const char *end_of_line);
-bool test_functions(const char *end_of_line);
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+#define assert(condition, fail_message)                                        \
+  if (!(condition)) {                                                          \
+    fflush(stdout);                                                            \
+    fprintf(stderr, ANSI_COLOR_RED "\uf00d %s\n" ANSI_COLOR_RESET,             \
+            fail_message);                                                     \
+    return 1;                                                                  \
+  }
+
+int test_basics(const char *end_of_line);
+int test_functions(const char *end_of_line);
 
 int *get_vector(int);
 bool put_vector(int *vector);
 void append_to_vector(int *vector, int);
+void display_success(const char *end_of_line);
 
 int main() {
   char *end_of_line = "\n\t";
+  int err = 0;
 
   printf("\nTests: Basics\n\t");
-  test_basics(end_of_line);
+  err |= test_basics(end_of_line);
 
   printf("\nTests: Functions\n\t");
-  test_functions(end_of_line);
+  err |= test_functions(end_of_line); // only 0 | 0 is 0
+
+  return err;
 }
 
-bool test_basics(const char *end_of_line) {
+int test_basics(const char *end_of_line) {
   const int max_size = 2;
 
   printf("test: vector_create_with_capacity                    \t");
   double *n = NULL;
   vector_create_with_capacity(n, max_size);
-  assert(n != NULL && "vector after create is still NULL");
-  printf("[SUCCESS]%s", end_of_line);
+  assert(n != NULL, "vector after create is still NULL");
+  display_success(end_of_line);
 
   printf("test: vector_append                                  \t");
   vector_append(n, 1.5);
-  printf("[SUCCESS]%s", end_of_line);
+  display_success(end_of_line);
 
   printf("test: vector_len                                     \t");
-  assert(vector_len(n) == 1 && "vector_len is not 1");
-  printf("[SUCCESS]%s", end_of_line);
+  assert(vector_len(n) == 1, "vector_len is not 1");
+  display_success(end_of_line);
 
   printf("test: vector_free                                    \t");
   vector_free(n);
-  printf("[SUCCESS]%s", end_of_line);
+  display_success(end_of_line);
 
   printf("test: capacity is increased if it overflows          \t");
   int *n1;
@@ -47,7 +62,7 @@ bool test_basics(const char *end_of_line) {
   vector_append(n1, 1);
   vector_append(n1, 2);
   vector_append(n1, 3);
-  printf("[SUCCESS]%s", end_of_line);
+  display_success(end_of_line);
 
   vector_free(n1);
 
@@ -56,21 +71,21 @@ bool test_basics(const char *end_of_line) {
   vector_append(n2, 1);
   vector_append(n2, 2);
   vector_append(n2, 3);
-  printf("[SUCCESS]%s", end_of_line);
+  display_success(end_of_line);
 
   printf("test: access elements like an array                  \t");
   int sum = 0;
   for (int i = 0; i < vector_len(n2); i++) {
     sum += n2[i];
   }
-  assert(sum == 6 && "sum of elements is not 6");
-  printf("[SUCCESS]%s", end_of_line);
+  assert(sum == 6, "sum of elements should be 6");
+  display_success(end_of_line);
 
   printf("test: remove an element from the end of the array    \t");
   vector_remove_last(n2);
-  assert(vector_len(n2) == 2 && "lens do not match");
-  assert(n2[0] == 1 && n2[1] == 2 && "values do not match");
-  printf("[SUCCESS]%s", end_of_line);
+  assert(vector_len(n2) == 2, "lens do not match");
+  assert(n2[0] == 1 && n2[1] == 2, "values do not match");
+  display_success(end_of_line);
 
   printf("test: remove an element from the middle of the array \t");
   vector_append(n2, 3);
@@ -78,23 +93,27 @@ bool test_basics(const char *end_of_line) {
   vector_append(n2, 5);
 
   vector_remove(n2, 2); // remove the 3rd element
-  assert(vector_len(n2) == 4 && "lens do not match");
-  assert(n2[0] == 1 && n2[1] == 2 && n2[2] == 4 && n2[3] == 5 &&
+  assert(vector_len(n2) == 4, "lens do not match");
+  assert(n2[0] == 1 && n2[1] == 2 && n2[2] == 4 && n2[3] == 5,
          "values do not match");
-  printf("[SUCCESS]%s", end_of_line);
+  display_success(end_of_line);
 
   vector_free(n2);
+
+  return 0;
 }
 
-bool test_functions(const char *end_of_line) {
+int test_functions(const char *end_of_line) {
   printf("test: calling a function which returns a vector   \t");
   int *v = get_vector(4);
-  assert(v != NULL && "returned vector is null");
-  printf("[SUCCESS]%s", end_of_line);
+  assert(v != NULL, "returned vector is null");
+  display_success(end_of_line);
 
   printf("test: calling a function which takes a vector     \t");
-  assert(put_vector(v) && "vector in a function is null");
-  printf("[SUCCESS]%s", end_of_line);
+  assert(put_vector(v), "vector in a function is null");
+  display_success(end_of_line);
+
+  return 0;
 }
 
 int *get_vector(int n) {
@@ -104,3 +123,7 @@ int *get_vector(int n) {
 }
 
 bool put_vector(int *vector) { return vector != NULL; }
+
+void display_success(const char *end_of_line) {
+  printf(ANSI_COLOR_GREEN "\uf42e%s" ANSI_COLOR_RESET, end_of_line);
+}
